d3921122f58a0d7fca6a2cebf92ef15999dbe9fa
Jonathan <jonathan@chattyhive.com>
2014-02-21 21:59:20 +0100

    GUI - SWIPE implemented.
    Note that if user starts movement from main panel, and at the center screen and goes a little to one side and then returns with some speed, releasing the screen after passing the middle of the screen, then the panel of the opposite side than first showed is shown. This behaviour is not an anomaly or malfunction since other applications have the same behaviour, outstanding Google Play Store.
    
    ATTENTION!: Background service and broadcast receivers have been disabled for battery saving purposes while testing in real devices.
    
             2014-02-21T21:58:50A

--- ChattyHive/src/main/java/com/chattyhive/chattyhive/Main.java
+++ ChattyHive/src/main/java/com/chattyhive/chattyhive/Main.java
@@ -3,10 +3,13 @@ package com.chattyhive.chattyhive;
 import android.app.Activity;
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
@@ -17,13 +20,20 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-public class Main extends Activity {
+public class Main extends Activity implements GestureDetector.OnGestureListener {
+
+    GestureDetector detector;
+    MotionEvent _lastOnScrollMotionEvent = null;
+    MotionEvent _firstOnScrollMotionEvent = null;
+    Boolean _performScroll = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main);
+
+        detector = new GestureDetector(this,this);
 
         LinearLayout main_block = (LinearLayout)findViewById(R.id.main_block);
         FrameLayout.LayoutParams main_block_params = (FrameLayout.LayoutParams) main_block.getLayoutParams();
@@ -40,46 +50,26 @@ public class Main extends Activity {
 
                 final LinearLayout main_block = (LinearLayout)findViewById(R.id.main_block);
                 final FrameLayout.LayoutParams main_block_params = (FrameLayout.LayoutParams) main_block.getLayoutParams();
-                final int translate, new_margin_left, new_margin_right;
+                final int translate, new_left_margin, new_right_margin;
 
                 int main_block_left_margin = main_block_params.leftMargin;
                 int main_block_right_margin = main_block_params.rightMargin;
 
-                if (main_block_left_margin == 0) {
+                int distance = (int)getResources().getDimension(R.dimen.chat_list_width);
+
+                if (main_block_right_margin < 0) {
                     translate = main_block_right_margin;
-                    new_margin_left = main_block_right_margin;
-                    new_margin_right = 0;
+                    new_left_margin = -1 * distance;
+                    new_right_margin = 0;
                 } else {
                     translate = -1*main_block_left_margin;
-                    new_margin_left = 0;
-                    new_margin_right = main_block_left_margin;
+                    new_left_margin = 0;
+                    new_right_margin = -1 * distance;
                 }
 
-                Animation animation = new TranslateAnimation(0, translate,0, 0);
-                animation.setDuration(600);
-                animation.setFillAfter(true);
 
-                animation.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
 
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        main_block_params.setMargins(new_margin_left,main_block_params.topMargin,new_margin_right,main_block_params.bottomMargin);
-                        main_block.destroyDrawingCache();
-                        main_block.setLayoutParams(main_block_params);
-                        main_block.clearAnimation();
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-
-                    }
-                });
-
-                main_block.startAnimation(animation);
+                movePanel(main_block,main_block_params,translate,new_left_margin,new_right_margin,250);
             }
         });
 
@@ -90,49 +80,46 @@ public class Main extends Activity {
 
                 final LinearLayout main_block = (LinearLayout)findViewById(R.id.main_block);
                 final FrameLayout.LayoutParams main_block_params = (FrameLayout.LayoutParams) main_block.getLayoutParams();
-                final int translate, new_margin_left, new_margin_right;
+                final int translate, new_left_margin, new_right_margin;
 
                 int main_block_left_margin = main_block_params.leftMargin;
+                int main_block_right_margin = main_block_params.rightMargin;
 
-                if (main_block_left_margin == (-1*getResources().getDimension(R.dimen.chat_list_width))) {
-                    translate = (int)(-1*getResources().getDimension(R.dimen.chat_list_width));
-                    new_margin_left = main_block_left_margin+translate;
-                    new_margin_right = (int)getResources().getDimension(R.dimen.chat_list_width);
+                int distance = (int)getResources().getDimension(R.dimen.right_menu_width);
+                int zero_left = (int)getResources().getDimension(R.dimen.chat_list_width);
+                if (main_block_right_margin > 0) {
+                    translate = main_block_right_margin;
+                    new_left_margin = -1 * zero_left;
+                    new_right_margin = 0;
                 } else {
-                    translate = (int)getResources().getDimension(R.dimen.chat_list_width);
-                    new_margin_left = main_block_left_margin+translate;
-                    new_margin_right = 0;
+                    translate = -1 * (distance - main_block_right_margin);
+                    new_left_margin = -1*zero_left - distance;
+                    new_right_margin = distance;
                 }
 
-                Animation animation = new TranslateAnimation(0, translate,0, 0);
-                animation.setDuration(600);
-                animation.setFillAfter(true);
 
-                animation.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
 
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        main_block_params.setMargins(new_margin_left,main_block_params.topMargin,new_margin_right,main_block_params.bottomMargin);
-                        main_block.destroyDrawingCache();
-                        main_block.setLayoutParams(main_block_params);
-                        main_block.clearAnimation();
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-
-                    }
-                });
-
-                main_block.startAnimation(animation);
+                movePanel(main_block,main_block_params,translate,new_left_margin,new_right_margin,250);
             }
         });
     }
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent me){
+        // TODO: ACTION_UP must be detected here.
+        if (!this.detector.onTouchEvent(me)) {
+            if ((_performScroll) && (me.getAction() == MotionEvent.ACTION_UP)) {
+                float distanceX, distanceY;
+                distanceX = _lastOnScrollMotionEvent.getX() - me.getX();
+                distanceY = _lastOnScrollMotionEvent.getY() - me.getY();
+                return this.onScroll(_firstOnScrollMotionEvent,me,distanceX,distanceY);
+            }
+            else {
+                return super.dispatchTouchEvent(me);
+            }
+        }
+        return true;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -152,5 +139,199 @@ public class Main extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void movePanel(final LinearLayout main_block, final FrameLayout.LayoutParams main_block_params, final int translate, final int new_margin_left, final int new_margin_right,long duration) {
+
+        Animation animation = new TranslateAnimation(0, translate,0, 0);
+        animation.setDuration(duration);
+        animation.setFillAfter(true);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                main_block_params.setMargins(new_margin_left,main_block_params.topMargin,new_margin_right,main_block_params.bottomMargin);
+                main_block.destroyDrawingCache();
+                main_block.setLayoutParams(main_block_params);
+                main_block.clearAnimation();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        main_block.startAnimation(animation);
+    }
+
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+
+        LinearLayout main_block = (LinearLayout)findViewById(R.id.main_block);
+        FrameLayout.LayoutParams main_block_params = (FrameLayout.LayoutParams) main_block.getLayoutParams();
+
+        if (!_performScroll) {
+            int actual_margin_right = main_block_params.rightMargin;
+
+            if (actual_margin_right < 0) { // left panel is visible
+                if (e1.getX() < -1*actual_margin_right) return false;
+            } else if (actual_margin_right > 0) { // right panel is visible
+                DisplayMetrics metrics = new DisplayMetrics();
+                this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+                int screen_width = metrics.widthPixels;
+                Log.w("onScroll","Screen width: ".concat(String.valueOf(screen_width)));
+                if (e1.getX() > (screen_width-actual_margin_right)) return false;
+            }
+        }
+
+        if (_lastOnScrollMotionEvent == null) {
+            _lastOnScrollMotionEvent = e2;
+            if (Math.abs(distanceY) < Math.abs(distanceX)) {
+                _performScroll = true;
+                _firstOnScrollMotionEvent = e1;
+            }
+        }
+
+        if (!_performScroll) {
+            return false;
+        }
+
+        _lastOnScrollMotionEvent = e2;
+
+        int max_scroll_right = (int)getResources().getDimension(R.dimen.chat_list_width);
+        int max_scroll_left = (int)getResources().getDimension(R.dimen.right_menu_width);
+
+        int margin_left = Math.round(main_block_params.leftMargin - distanceX);
+        int margin_right = Math.round(main_block_params.rightMargin + distanceX);
+
+        if (margin_right > max_scroll_left) {
+            margin_right = max_scroll_left;
+            margin_left = -1 * (max_scroll_left+max_scroll_right);
+            //Log.w("onScroll", "Movement locked at left side.");
+        } else if (margin_left > 0) {
+            margin_right = -1*max_scroll_right;
+            margin_left = 0;
+            //Log.w("onScroll", "Movement locked at right side.");
+        }
+
+        //Log.w("onScroll","Previous left:  ".concat(String.valueOf(main_block_params.leftMargin)).concat(" New left:  ").concat(String.valueOf(margin_left)));
+        //Log.w("onScroll","Previous right: ".concat(String.valueOf(main_block_params.rightMargin)).concat(" New right: ").concat(String.valueOf(margin_right)));
+        //Log.w("onScroll","Distance X: ".concat(String.valueOf(distanceX)));
+
+        main_block_params.setMargins(margin_left, main_block_params.topMargin, margin_right, main_block_params.bottomMargin);
+        main_block.setLayoutParams(main_block_params);
+
+        if (e2.getAction() == MotionEvent.ACTION_UP) {
+            _performScroll = false;
+            _lastOnScrollMotionEvent = null;
+            _firstOnScrollMotionEvent = null;
+
+            int chat_list_width = (int)getResources().getDimension(R.dimen.chat_list_width);
+            int right_menu_width = (int)getResources().getDimension(R.dimen.right_menu_width);
+            int translate, new_margin_right, new_margin_left;
+
+            if ((margin_right >= (-0.5*chat_list_width)) && (margin_right <= (0.5*right_menu_width))) { // Show center panel (main window)
+                new_margin_right = 0;
+            } else if (margin_right > (0.5*right_menu_width)) { // Show right panel (menu)
+                new_margin_right = right_menu_width;
+            } else { // Show left panel (chat list)
+                new_margin_right = -1*chat_list_width;
+            }
+            translate = margin_right - new_margin_right;
+            new_margin_left = margin_left + translate;
+
+            movePanel(main_block,main_block_params,translate,new_margin_left,new_margin_right,250);
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        if ((e2.getAction() != MotionEvent.ACTION_UP) || (Math.abs(velocityX) < Math.abs(velocityY))) return false;
+
+        if (Math.abs(velocityX) < 200) return false;
+
+        Log.w("onFling","onFlingDetected!");
+
+        float finger_distance = e1.getX() - e2.getX();
+
+        LinearLayout main_block = (LinearLayout)findViewById(R.id.main_block);
+        FrameLayout.LayoutParams main_block_params = (FrameLayout.LayoutParams) main_block.getLayoutParams();
+
+        int margin_right = main_block_params.rightMargin;
+        int margin_left = main_block_params.leftMargin;
+
+        int origin_right = Math.round(margin_right - finger_distance);
+
+        if (origin_right < 0) { // left panel is visible
+            if (e1.getX() < -1*origin_right) return false;
+        } else if (origin_right > 0) { // right panel is visible
+            DisplayMetrics metrics = new DisplayMetrics();
+            this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            int screen_width = metrics.widthPixels;
+            if (e1.getX() > (screen_width-origin_right)) return false;
+        }
+
+        int chat_list_width = (int)getResources().getDimension(R.dimen.chat_list_width);
+        int right_menu_width = (int)getResources().getDimension(R.dimen.right_menu_width);
+
+        int translate, new_margin_right, new_margin_left;
+
+
+        if ((origin_right >= (-0.5*chat_list_width)) && (origin_right <= (0.5*right_menu_width))) {
+            if ((velocityX > 0) && (margin_right < 0)) { // Show left panel (chat list)
+                new_margin_right = -1*chat_list_width;
+            } else if ((velocityX < 0) && (margin_right > 0)) { // Show right panel (menu)
+                new_margin_right = right_menu_width;
+            } else { // Ensure that main panel is shown
+                new_margin_right = 0;
+            }
+        } else if ((origin_right > (0.5*right_menu_width)) && (velocityX > 0)) { // Show center panel (main)
+            new_margin_right = 0;
+        } else if ((origin_right < (-0.5*chat_list_width)) && (velocityX < 0)) { // Show center panel (main)
+            new_margin_right = 0;
+        } else return false;
+
+        translate = margin_right - new_margin_right;
+        new_margin_left = margin_left + translate;
+
+        if (_performScroll) {
+            _performScroll = false;
+            _lastOnScrollMotionEvent = null;
+            _firstOnScrollMotionEvent = null;
+        }
+
+        movePanel(main_block,main_block_params,translate,new_margin_left,new_margin_right,100);
+
+        return true;
     }
 }
