65b7439af9dece06487c008ca7b72cb1e213ec28
jonathan-chattyhive <jonathan@chattyhive.com>
2014-10-14 14:42:06 +0200

    - Corrected age calculation in profile. - Corrected (Message sending: alpha 0.5. Message not confirmed: normal. Message confirmed: tick) - Corrected. Exception on joining group chats. - Corrected Chats and Explore buttons in Home. - Working on Message's timestamp.  --------------------------------------------------------------  - Randomly an exception occurs when a new message arrives. Randomness needs to be determined.  - Profile view doesn't fit well with long location name.  - Profile view doesn't fit well with long languages array.
       2014-10-14T14:41A

--- ChattyHive/src/main/java/com/chattyhive/chattyhive/ChatListAdapter.java
+++ ChattyHive/src/main/java/com/chattyhive/chattyhive/ChatListAdapter.java
@@ -3,6 +3,7 @@ package com.chattyhive.chattyhive;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +23,7 @@ import com.chattyhive.backend.contentprovider.server.ServerCommand;
 import com.chattyhive.backend.util.events.EventArgs;
 import com.chattyhive.backend.util.formatters.DateFormatter;
 import com.chattyhive.backend.util.formatters.TimestampFormatter;
+import com.chattyhive.chattyhive.framework.Util.StaticMethods;
 
 import java.util.Date;
 
@@ -101,6 +103,8 @@ public class ChatListAdapter extends BaseAdapter {
             holder = new ViewHolder();
             switch (chatKind) {
                 case HIVE:
+                case PUBLIC_GROUP:
+                case PRIVATE_GROUP:
                     if (type == context.getResources().getInteger(R.integer.MainPanelChat_ListKind_Me)) {
                         convertView = this.inflater.inflate(R.layout.main_panel_chat_hive_message_me, parent, false);
                         isMessage = true;
@@ -123,6 +127,7 @@ public class ChatListAdapter extends BaseAdapter {
                         holder.messageText = (TextView) convertView.findViewById(R.id.main_panel_chat_messageText);
                         holder.avatarThumbnail = (ImageView) convertView.findViewById(R.id.main_panel_chat_avatarThumbnail);
                         holder.timeStamp = (TextView) convertView.findViewById(R.id.main_panel_chat_timeStamp);
+                        holder.chatItem = convertView.findViewById(R.id.main_panel_chat_item);
                     }
                     break;
                 case PUBLIC_SINGLE:
@@ -147,6 +152,7 @@ public class ChatListAdapter extends BaseAdapter {
                     if (isMessage) {
                         holder.messageText = (TextView) convertView.findViewById(R.id.main_panel_chat_single_message_messageText);
                         holder.timeStamp = (TextView) convertView.findViewById(R.id.main_panel_chat_single_message_timeStamp);
+                        holder.chatItem = convertView.findViewById(R.id.main_panel_chat_item);
                     }
                     break;
                 default:
@@ -179,15 +185,22 @@ public class ChatListAdapter extends BaseAdapter {
             else if (message.getTimeStamp() != null)
                 holder.timeStamp.setText(TimestampFormatter.toLocaleString(message.getTimeStamp()));
             else*/
+            if (message.getOrdinationTimeStamp() != null)
+                holder.timeStamp.setText(TimestampFormatter.toLocaleString(message.getOrdinationTimeStamp()));
+            else
                 holder.timeStamp.setText(TimestampFormatter.toLocaleString(new Date()));
+
             if (message.getUser().isMe()) {
                 if (((this.chatKind == GroupKind.PRIVATE_SINGLE) || (this.chatKind == GroupKind.PUBLIC_SINGLE)) && (message.getConfirmed())) {
+                    StaticMethods.SetAlpha(holder.chatItem,1f);
                     holder.timeStamp.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.abc_ic_cab_done_holo_light,0);
                 } else if (message.getId() != null) {
-                    //holder.timeStamp.setCompoundDrawablesWithIntrinsicBounds(0,0,0,0);
+                    StaticMethods.SetAlpha(holder.chatItem,1f);
                 } else {
-                    holder.timeStamp.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.pestanha_hives_historial,0);
+                    StaticMethods.SetAlpha(holder.chatItem,0.5f);
                 }
+            } else {
+                StaticMethods.SetAlpha(holder.chatItem,1f);
             }
         } else if (!isHoleMarker) {
             if (message.getTimeStamp() != null)
@@ -196,13 +209,13 @@ public class ChatListAdapter extends BaseAdapter {
                 holder.timeStamp.setText(DateFormatter.toHumanReadableString(new Date()));
         } else {
             holder.messageText.setText(String.format("Loading %s messages...",message.getMessageContent().getContent()));
-            holder.messageText.setCompoundDrawablesWithIntrinsicBounds(R.drawable.menu_new_hive_blanco,0,0,0);
             message.FillMessageHole(this.channelChat.getMessageByIndex(position+1).getId());
         }
         return convertView;
     }
 
     private static class ViewHolder {
+        public View chatItem;
         public TextView username;
         public TextView messageText;
         public TextView timeStamp;
