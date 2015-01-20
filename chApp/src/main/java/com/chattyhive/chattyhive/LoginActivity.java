bc1b3827814db8bd3281ab5f093389c41b9bd83f
jonathan-chattyhive <jonathan@chattyhive.com>
2015-01-20 14:02:39 +0100

    Removing fake register mode from publicable versions.
    2015-01-20T14:02A

--- chApp/src/main/java/com/chattyhive/chattyhive/LoginActivity.java
+++ chApp/src/main/java/com/chattyhive/chattyhive/LoginActivity.java
@@ -26,7 +26,7 @@ public class LoginActivity extends Activity {
     static final int OP_CODE_REGISTER = 3;
 
     private Boolean connecting;
-    private Boolean fakeMode;
+    //private Boolean fakeMode;
     private DataProvider dataProvider;
     private Controller controller;
 
@@ -51,27 +51,27 @@ public class LoginActivity extends Activity {
             }
         });
 
-        findViewById(R.id.login_sign_up_with_facebook).setOnClickListener(new View.OnClickListener() {
+        /*findViewById(R.id.login_sign_up_with_facebook).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 facebookButtonFakeSignUp("jonathan","12345678");
             }
-        });
+        });*/
 
         connecting = false;
-        fakeMode = false;
+        //fakeMode = false;
 
             dataProvider = DataProvider.GetDataProvider();
             controller = Controller.GetRunningController();
             dataProvider.ServerConnectionStateChanged.add(new EventHandler<ConnectionEventArgs>(this,"onServerConnectionStateChanged",ConnectionEventArgs.class));
     }
 
-    public void facebookButtonFakeSignUp(String username, String password) {
+    /*public void facebookButtonFakeSignUp(String username, String password) {
         fakeMode = true;
         controller.LocalUserReceived.add(new EventHandler<EventArgs>(this,"onLocalUserLoaded",EventArgs.class));
         dataProvider.setUser(new ServerUser(username, password));
         dataProvider.Connect();
-    }
+    }*/
 
     protected void setTabButtonsBehaviour() {
         final Button signup = (Button)findViewById(R.id.login_activity_signup_tab_button);
@@ -171,21 +171,21 @@ public class LoginActivity extends Activity {
     }
 
     public void onLocalUserLoaded(Object sender,EventArgs eventArgs) {
-        if (fakeMode) {
+       /* if (fakeMode) {
             Intent intent = new Intent(this, Register.class);
             intent.putExtra("fake",true);
             startActivityForResult(intent, OP_CODE_REGISTER);
-        }
+        }*/
     }
 
     public void onServerConnectionStateChanged(Object sender,ConnectionEventArgs eventArgs) {
         connecting = false;
         //First hide animation
         if (eventArgs.getConnected()) {
-            if (!fakeMode) {
+            //if (!fakeMode) {
                 setResult(RESULT_OK);
                 finish();
-            }
+            //}
         } else {
             //Show some kind of error
             TextView usernameView = ((TextView)findViewById(R.id.login_activity_login_username));
